90f6a6cf727c531da429a8e1a017e6c8f52fa853
rajesh-pathak <rajesh.pathak@pnp-hcl.com>
2023-10-20 15:33:08 +0530

    added

--- src/test/java/e2e/uiAutomationNew/AppTest.java
+++ src/test/java/e2e/uiAutomationNew/AppTest.java
@@ -25,9 +25,18 @@ public class AppTest {
 	public void verifyUiLaunch() throws InterruptedException {
 		// Setup ChromeOptions to run in incognito mode and disable notifications
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--incognito");
+		//options.addArguments("--incognito");
 		options.addArguments("--disable-infobars");
 		options.addArguments("--use-fake-ui-for-media-stream"); // Enable microphone access
+		// Configure Chrome to// download files without a prompt
+        String downloadPath = "C:\\Users\\rajesh.pathak\\Desktop\\HCL docs"; // Set your desired download directory
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--safebrowsing-disable-download-protection");
+        options.addArguments("--download.default_directory=" + downloadPath);
+        options.addArguments("--no-pings"); // Disable pinging the server for downloads
 		// Initialize the ChromeDriver with the configured options
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver(options);
@@ -39,23 +48,30 @@ public class AppTest {
 		WebElement recordButton = driver.findElement(By.xpath("//div[@class='btn-record']"));
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click();", recordButton);
-		 // Schedule a task to keep checking recording status for one hour
-        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-        executorService.scheduleAtFixedRate(() -> {
-            // Check if recording is still in progress
-            WebElement stopButton = driver.findElement(By.xpath("//div[@class='btn-stop']"));
-            if (stopButton.isDisplayed()) {
-                System.out.println("Recording is still in progress.");
-            } else {
-                System.out.println("Recording has finished.");
-                // Optionally, you can perform additional checks or assertions here
-                Assert.assertTrue(true, "Recording finished successfully.");
-                executorService.shutdown(); // Stop the task
-            }
-        }, 0, 1, TimeUnit.MINUTES); // Check every minute
 
-        // Wait for one hour before completing the test
-        Thread.sleep(60 * 60 * 1000); // 1 hour in milliseconds
-
+		Thread.sleep(2000);
+		WebElement pause = driver.findElement(By.xpath("//div[@class='btn-record active']"));
+		WebElement save = driver.findElement(By.xpath("//div[@class='save-processing']"));
+		js.executeScript("arguments[0].click();", pause);
+		Thread.sleep(2000);
+		js.executeScript("arguments[0].click();", save);
+		Thread.sleep(2000);
+		/*
+		 * // Schedule a task to keep checking recording status for one hour
+		 * ScheduledExecutorService executorService =
+		 * Executors.newSingleThreadScheduledExecutor();
+		 * executorService.scheduleAtFixedRate(() -> { // Check if recording is still in
+		 * progress WebElement stopButton =
+		 * driver.findElement(By.xpath("//div[@class='btn-stop']")); if
+		 * (stopButton.isDisplayed()) {
+		 * System.out.println("Recording is still in progress."); } else {
+		 * System.out.println("Recording has finished."); // Optionally, you can perform
+		 * additional checks or assertions here Assert.assertTrue(true,
+		 * "Recording finished successfully."); executorService.shutdown(); // Stop the
+		 * task } }, 0, 1, TimeUnit.MINUTES); // Check every minute
+		 * 
+		 * // Wait for one hour before completing the test Thread.sleep(60 * 60 * 1000);
+		 * // 1 hour in milliseconds
+		 */
 	}
 }
